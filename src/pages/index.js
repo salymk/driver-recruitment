967a8a8a8e6b4b6e36702ed541d65891512f0d79
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-11-07 16:31:36 -0700

    Install dependencies
    - headless ui
    - hero-icons
    - copy over custom css
    - copy over index page and 404 page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,46 @@
 import * as React from 'react';
+import Layout from '../components/layout';
+import { Hero } from '../components/hero';
+import { WhyDriveForUs } from '../components/why-drive-for-us';
+import { EmployeeVSOwner } from '../components/employee-vs-owner';
+import { Brands } from '../components/brands';
+import { FAQ } from '../components/FAQ';
+import { Testimonials } from '../components/testimonials';
+import { ApplicationProcess } from '../components/application-process';
+import { JobOpenings } from '../components/job-openings';
 
 const IndexPage = () => (
-  <>
-    <h1>Hello world</h1>
-  </>
+  <Layout>
+    {/* Hero */}
+    <Hero />
+
+    {/* Who are we? and What do we do? */}
+
+    {/* Why work with us? */}
+    <WhyDriveForUs />
+
+    {/* How we work with Drivers and Owner Operators */}
+    <EmployeeVSOwner />
+
+    {/* Brands we work with logos */}
+    <Brands />
+
+    {/* Testimonials */}
+    <Testimonials />
+
+    {/* How to get started */}
+    <ApplicationProcess />
+
+    {/* FAQ */}
+    <FAQ />
+
+    <JobOpenings />
+    {/* Open Positions */}
+    {/* Employee  */}
+    {/* Link to tentstreet */}
+    {/* Owner/operator */}
+    {/* Calendly Link */}
+  </Layout>
 );
 
 export default IndexPage;
