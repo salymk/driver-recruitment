f286749bc7f95f8e88d70f44e9062c4d7481b3b9
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-11-07 16:34:39 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,10 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Alumarain Trucking`,
+    description: `Come work with the best trucking company in the mid west`,
+    author: `Umar Mitchell`,
+    siteUrl: `https://jobs.alumarainco.com`,
   },
   plugins: [
     {
@@ -31,15 +31,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `AlUmarain Trucking`,
+        short_name: `AlUmarain`,
         start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        background_color: `#1A202C`,
+        theme_color: `#1A202C`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
